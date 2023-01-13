38a69fe8853e8a3ddfabafbaadf82aea504dcc8f
zhanghaibo <1091675746@qq.com>
2023-01-13 14:55:43 +0800

    fix:Cluster  client

---
+++ src/RedisNaruto/Internal/Cluster/ClusterRedisClient.cs
@@ -0,0 +1,25 @@
+using System.Net.Sockets;
+using RedisNaruto.Internal.Interfaces;
+
+namespace RedisNaruto.Internal.Cluster;
+
+/// <summary>
+/// 集群客户端
+/// </summary>
+internal class ClusterRedisClient : RedisClient
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public ClusterRedisClient(TcpClient tcpClient, ConnectionModel connectionModel, string currentHost,
+        int currentPort,
+        Func<IRedisClient, Task> disposeTask) : base(tcpClient, connectionModel, currentHost, currentPort, disposeTask)
+    {
+    }
+
+    public override Task<bool> SelectDb(int db)
+    {
+        //集群只有一个db 不允许切换数据库
+        return Task.FromResult(true);
+    }
+}
