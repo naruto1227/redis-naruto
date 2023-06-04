74e0237a38cf9dd3ae4cb3ed02e86f235da69133
zhanghaibo <1091675746@qq.com>
2023-06-04 21:28:15 +0800

    Parse

--- src/RedisNaruto/ConnectionBuilder.cs
+++ src/RedisNaruto/ConnectionBuilder.cs
@@ -1,3 +1,5 @@
+using RedisNaruto.Utils;
+
 namespace RedisNaruto;
 
 /// <summary>
@@ -53,7 +55,7 @@ public sealed class ConnectionBuilder
     /// <summary>
     /// 最大
     /// </summary>
-    private readonly int _maxPoolCount;
+    private int _maxPoolCount;
 
     /// <summary>
     /// 最大连接池
@@ -65,9 +67,9 @@ public sealed class ConnectionBuilder
             if (_maxPoolCount == 0) return PoolCount * 2;
             return _maxPoolCount;
         }
-        init => _maxPoolCount = value;
+        set => _maxPoolCount = value;
     }
-    
+
 
     /// <summary>
     /// 超时时间 ms 默认3000
@@ -79,4 +81,76 @@ public sealed class ConnectionBuilder
     /// 闲置时间 闲置多久进行释放 ，当池中的数量低于 最小不做处理
     /// </summary>
     public int Idle { get; set; }
+
+    /// <summary>
+    /// 拆分的分隔符
+    /// </summary>
+    private const char SplitChar = ',';
+
+    /// <summary>
+    /// 相等
+    /// </summary>
+    private const char EqualChar = '=';
+
+    /// <summary>
+    /// 配置
+    /// </summary>
+    /// <param name="config"></param>
+    internal ConnectionBuilder Parse(string config)
+    {
+        var configs = config.Split(SplitChar);
+        if (configs is not {Length: > 0})
+        {
+            throw new ArgumentNullException(nameof(config));
+        }
+
+        //连接地址
+        var connection = new HashSet<string>();
+        //遍历
+        foreach (var itemEle in configs.Select(a => a.Trim()))
+        {
+            //判断是否是 主机地址
+            if (itemEle.IndexOf(EqualChar) > 0)
+            {
+                var ele = itemEle.Split(EqualChar);
+                switch (ele[0])
+                {
+                    case "username":
+                        this.UserName = ele[1];
+                        break;
+                    case "password":
+                        this.Password = ele[1];
+                        break;
+                    case "servertype":
+                        this.ServerType = (ServerType) ele[1].ToInt();
+                        break;
+                    case "mastername":
+                        this.MasterName = ele[1];
+                        break;
+                    case "database":
+                        this.DataBase = ele[1].ToInt();
+                        break;
+                    case "poolcount":
+                        this.PoolCount = ele[1].ToInt();
+                        break;
+                    case "maxpoolcount":
+                        this.MaxPoolCount = ele[1].ToInt();
+                        break;
+                    case "timeout":
+                        this.TimeOut = ele[1].ToInt();
+                        break;
+                    case "idle":
+                        this.Idle = ele[1].ToInt();
+                        break;
+                }
+            }
+            else
+            {
+                connection.Add(itemEle);
+            }
+        }
+
+        this.Connection = connection.ToArray();
+        return this;
+    }
 }
--- src/RedisNaruto/RedisConnection.cs
+++ src/RedisNaruto/RedisConnection.cs
@@ -11,6 +11,20 @@ public static class RedisConnection
     /// <returns></returns>
     public static async Task<IRedisCommand> CreateAsync(ConnectionBuilder config)
     {
+        if (config.MaxPoolCount < config.PoolCount)
+        {
+            throw new ArgumentException($"{nameof(config.MaxPoolCount)}不能小于最小池数量");
+        }
+
         return await RedisCommands.RedisCommand.BuilderAsync(config);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public static async Task<IRedisCommand> CreateAsync(string config)
+    {
+        return await CreateAsync(new ConnectionBuilder().Parse(config));
     }
 }
