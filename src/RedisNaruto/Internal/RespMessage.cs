1ad72ef6f9868dc51f69a7c3385f8d73653cd0a7
zhanghaibo <1091675746@qq.com>
2024-01-06 15:42:34 +0800

    fix:追加resp3 协议常量

--- src/RedisNaruto/Internal/RespMessage.cs
+++ src/RedisNaruto/Internal/RespMessage.cs
@@ -30,4 +30,54 @@ public static class RespMessage
     /// 数组
     /// </summary>
     public const char ArrayString = '*';
+
+    #region resp3
+
+    /// <summary>
+    /// https://redis.io/docs/reference/protocol-spec/#nulls
+    /// </summary>
+    public const char Nulls = '_';
+
+    /// <summary>
+    /// https://redis.io/docs/reference/protocol-spec/#booleans
+    /// </summary>
+    public const char Bool = '#';
+
+    /// <summary>
+    /// https://redis.io/docs/reference/protocol-spec/#doubles
+    /// </summary>
+    public const char Double = ',';
+
+    /// <summary>
+    /// Big numbers
+    /// https://redis.io/docs/reference/protocol-spec/#big-numbers
+    /// </summary>
+    public const char BigNumber = '(';
+
+    /// <summary>
+    /// https://redis.io/docs/reference/protocol-spec/#bulk-errors
+    /// </summary>
+    public const char BuckError = '!';
+
+    /// <summary>
+    /// https://redis.io/docs/reference/protocol-spec/#verbatim-strings
+    /// </summary>
+    public const char VerString = '=';
+
+    /// <summary>
+    /// https://redis.io/docs/reference/protocol-spec/#maps
+    /// </summary>
+    public const char Maps = '%';
+
+    /// <summary>
+    /// https://redis.io/docs/reference/protocol-spec/#sets
+    /// </summary>
+    public const char Sets = '~';
+
+    /// <summary>
+    /// https://redis.io/docs/reference/protocol-spec/#pushes
+    /// </summary>
+    public const char Pushs = '>';
+
+    #endregion
 }
