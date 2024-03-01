827951776a6de3a3fcf1297107b794343cc7f94e
zhanghaibo <1091675746@qq.com>
2024-03-01 13:04:19 +0800

    option

--- src/RedisNaruto/Models/ClientSideCachingOption.cs
+++ src/RedisNaruto/Models/ClientSideCachingOption.cs
@@ -2,5 +2,13 @@ namespace RedisNaruto.Models;
 
 public class ClientSideCachingOption
 {
-    
+    /// <summary>
+    /// key的匹配方式
+    /// </summary>
+    public Func<string, bool> KeyPrefixMatch { get; set; }
+
+    /// <summary>
+    /// 缓存的过期时间
+    /// </summary>
+    public TimeSpan TimeOut { get; set; }
 }
