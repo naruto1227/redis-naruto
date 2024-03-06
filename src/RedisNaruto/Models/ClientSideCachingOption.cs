7d39d9d12d3b7ab51575389c17f9d10ada4dcdb2
zhanghaibo <1091675746@qq.com>
2024-03-06 09:29:11 +0800

    capacity

--- src/RedisNaruto/Models/ClientSideCachingOption.cs
+++ src/RedisNaruto/Models/ClientSideCachingOption.cs
@@ -11,4 +11,8 @@ public class ClientSideCachingOption
     /// 缓存的过期时间
     /// </summary>
     public TimeSpan TimeOut { get; set; }
+    /// <summary>
+    /// 缓存中的容量限制，缓存的集合超过此大小将按照时间剔除老的数据
+    /// </summary>
+    public int Capacity { get; set; }
 }
