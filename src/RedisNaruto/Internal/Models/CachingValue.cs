3518157f8635d57f9245026e80c78ddbddf5e21f
zhanghaibo <1091675746@qq.com>
2024-03-07 18:17:02 +0800

    IncrementUseCount

--- src/RedisNaruto/Internal/Models/CachingValue.cs
+++ src/RedisNaruto/Internal/Models/CachingValue.cs
@@ -5,17 +5,34 @@ namespace RedisNaruto.Internal.Models;
 /// </summary>
 public class CachingValue
 {
-
-    
     public CachingValue(object value)
     {
         this.Value = value;
-        CreateTime=DateTime.Now;
+        CreateTime = DateTime.Now;
     }
 
     /// <summary>
     /// 值
     /// </summary>
     public object Value { get; init; }
-    public DateTime CreateTime { get;}
+
+    public DateTime CreateTime { get; }
+
+    /// <summary>
+    /// 使用次数 每次获取完毕后更新此字段信息 可用于 后续的缓存淘汰策略
+    /// </summary>
+    public int UseCount => _useCount;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private int _useCount;
+
+    /// <summary>
+    /// 递增 使用次数
+    /// </summary>
+    public void IncrementUseCount()
+    {
+        Interlocked.Increment(ref _useCount);
+    }
 }
