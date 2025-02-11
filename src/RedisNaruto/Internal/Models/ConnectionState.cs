6048b9ea37b13123c183df79e91c4de19da93441
zhanghaibo <1091675746@qq.com>
2025-02-11 17:32:59 +0800

    Console

--- src/RedisNaruto/Internal/Models/ConnectionState.cs
+++ src/RedisNaruto/Internal/Models/ConnectionState.cs
@@ -32,6 +32,7 @@ internal class ConnectionState
     {
         this.State = ConnectionStateEnum.Valid;
         this.InValidReason = string.Empty;
+        System.Console.WriteLine($"连接恢复:host={this.Host},port={this.Port}");
     }
 
     /// <summary>
@@ -41,6 +42,7 @@ internal class ConnectionState
     {
         this.State = ConnectionStateEnum.InValid;
         this.InValidReason = inValidReason;
+        System.Console.WriteLine($"连接失效:host={this.Host},port={this.Port}");
     }
 }
 
