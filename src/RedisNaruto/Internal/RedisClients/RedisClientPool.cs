38e39141b22631ae39efc52fcef490564bc1ea0b
zhanghaibo <1091675746@qq.com>
2025-03-02 23:36:34 +0800

    连接池优化

--- src/RedisNaruto/Internal/RedisClients/RedisClientPool.cs
+++ src/RedisNaruto/Internal/RedisClients/RedisClientPool.cs
@@ -32,29 +32,35 @@ internal sealed class RedisClientPool : IRedisClientPool
     /// 
     /// </summary>
     private IRedisClientFactory _redisClientFactory;
+    //
+    // /// <summary>
+    // /// 创建的总客户端总数
+    // /// </summary>
+    // private int _totalClientCount;
 
     /// <summary>
-    /// 创建的总客户端总数
+    /// 使用的数量
     /// </summary>
-    private int _totalClientCount;
+    private int _userCount;
 
     /// <summary>
     /// 闲置时间 闲置多久进行释放 ，当池中的数量低于 最小不做处理
     /// </summary>
     private readonly int _idle;
-    
+
     /// <summary>
     /// 
     /// </summary>
     private RedisClientPool(ConnectionBuilder connectionBuilder)
     {
-        _totalClientCount = 0;
+        // _totalClientCount = 0;
+        _userCount = 0;
         _minCount = connectionBuilder.PoolCount;
         _maxCount = connectionBuilder.MaxPoolCount;
         _idle = connectionBuilder.Idle;
         // _redisClientFactory = new RedisClientFactory(connectionBuilder);
     }
-    
+
     //构建
     public static async Task<RedisClientPool> BuildAsync(ConnectionBuilder connectionBuilder)
     {
@@ -80,27 +86,52 @@ internal sealed class RedisClientPool : IRedisClientPool
             {
                 break;
             }
+
             //从队列中获取
             if (_freeClients.TryDequeue(out var redisClient))
             {
                 //池内 空闲数减少
                 Interlocked.Decrement(ref _freeCount);
-                // 重置连接
-                await redisClient.ResetAsync(cancellationToken);
-                return redisClient;
+                Interlocked.Increment(ref _userCount);
+                Console.WriteLine($"从空闲池中读取,_freeCount={_freeCount},_userCount={_userCount}");
+
+                try
+                {
+                    // 重置连接
+                    await redisClient.ResetAsync(cancellationToken);
+                    return redisClient;
+                }
+                catch (Exception e)
+                {
+                    Interlocked.Decrement(ref _userCount);
+                    Console.WriteLine($"从空闲池中读取 ResetAsync 报错,_freeCount={_freeCount},_userCount={_userCount}");
+                    throw;
+                }
             }
 
             //判断 是否达到最大的池数量 达到了最大的话 就等待
-            if (Interlocked.Increment(ref _totalClientCount) <= _maxCount)
-                return await _redisClientFactory.GetAsync(
-                     this.Return,
-                    cancellationToken);
-            //还原
-            Interlocked.Decrement(ref _totalClientCount);
-            //todo 自旋等待 还是抛出？
-            //todo 这里的等待时间 通过配置
-            //todo 是否要一直等待 还是等待指定
-            await Task.Delay(500, cancellationToken);
+            if (Interlocked.Increment(ref _userCount) <= _maxCount)
+            {
+                Console.WriteLine($"创建新的连接 {_userCount}");
+                try
+                {
+                    var res = await _redisClientFactory.GetAsync(
+                        this.Return,
+                        cancellationToken);
+                    // Interlocked.Increment(ref _userCount);
+                    return res;
+                }
+                catch (Exception e)
+                {
+                    Interlocked.Decrement(ref _userCount);
+                    Console.WriteLine($"创建新的连接 {_userCount} 报错");
+                    throw;
+                }
+            }
+            //还原使用数
+            Interlocked.Decrement(ref _userCount);
+            
+            await Task.Delay(100, cancellationToken);
         }
 
         throw new OperationCanceledException("没有可用的连接");
@@ -115,9 +146,9 @@ internal sealed class RedisClientPool : IRedisClientPool
         {
             try
             {
-                var redisClient = await _redisClientFactory.GetAsync( this.Return);
-                //递增
-                Interlocked.Increment(ref _totalClientCount);
+                var redisClient = await _redisClientFactory.GetAsync(this.Return);
+                // //递增
+                // Interlocked.Increment(ref _totalClientCount);
                 //递增
                 Interlocked.Increment(ref _freeCount);
                 _freeClients.Enqueue(redisClient);
@@ -136,12 +167,16 @@ internal sealed class RedisClientPool : IRedisClientPool
     /// <returns></returns>
     private void Return([NotNull] IRedisClient redisClient)
     {
+        Interlocked.Decrement(ref _userCount);
+        Console.WriteLine($"Return begin _userCount {_userCount}");
+
         //返回队列的时候，判断下连接是否是断开的，如果是断开的话，就释放这个资源
         if (redisClient.IsClose)
         {
             redisClient.Close();
             return;
         }
+
         Interlocked.Increment(ref _freeCount);
         //入队
         _freeClients.Enqueue(redisClient);
@@ -157,18 +192,18 @@ internal sealed class RedisClientPool : IRedisClientPool
         {
             return;
         }
-
+    
         //todo 时间配置
         using var periodicTimer = new PeriodicTimer(TimeSpan.FromMinutes(3));
         while (await periodicTimer.WaitForNextTickAsync())
         {
             //只有超过 最小数才裁剪
-            var excessCount = _totalClientCount - _minCount;
+            var excessCount = _userCount - _minCount;
             if (excessCount <= 0)
             {
                 continue;
             }
-
+    
             //获取当前时间戳
             var timestamp = TimeUtil.GetTimestamp();
             if (_freeClients.IsEmpty) continue;
@@ -186,10 +221,10 @@ internal sealed class RedisClientPool : IRedisClientPool
                         _freeClients.Enqueue(redisClient2);
                         continue;
                     }
-
+    
                     redisClient2.Close();
                     Interlocked.Decrement(ref _freeCount);
-                    Interlocked.Decrement(ref _totalClientCount);
+                    Interlocked.Decrement(ref _userCount);
                 }
                 else
                 {
